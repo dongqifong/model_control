66ad80b9465732cd34a4859c65a9edbe8d33c19f
qifong <qifong04@gmail.com>
2022-01-01 23:12:57 +0800

    [Update] Update

---
+++ model_control/model_controller/retrain_clf/make_data_loader.py
@@ -0,0 +1,58 @@
+from numpy import ndarray
+import numpy as np
+from sklearn.utils import shuffle
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+
+class ModelDataset(Dataset):
+    def __init__(self, x: np.ndarray, y: np.ndarray) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+
+    def __getitem__(self, index):
+        x = torch.tensor(self.x[index:index+1, :]).float()
+        y = torch.tensor(self.y[index]).float()
+        return x, y
+
+    def __len__(self):
+        return len(self.x)
+
+
+class MakeLoader:
+    def __init__(self, x, y, config={}) -> None:
+        shuffle = config["shuffle"] if "shuffle" in config.keys() else True
+        batch_size = config["batch_size"] if "batch_size" in config.keys(
+        ) else 5
+        self.dataset = ModelDataset(x, y)
+        self.dataloader = DataLoader(
+            self.dataset, batch_size=batch_size, shuffle=shuffle)
+        return None
+
+    def get_loader(self):
+        return self.dataloader
+
+
+if __name__ == "__main__":
+    from test_model import ModelMonitor
+    model = ModelMonitor()
+
+    n_sample = 30
+    input_size = 51200
+    xx = np.random.random((n_sample, input_size))
+    yy = np.random.random(n_sample,)
+
+    dataloader = MakeLoader(xx, yy).get_loader()
+
+    with torch.no_grad():
+        for epoch in range(10):
+            for x, y in dataloader:
+                print("x_shape=", x.shape)
+                print("y_shape=", y.shape)
+                out = model(x)
+                break
+            break
+    print(out.shape)
+    print(y.shape)
